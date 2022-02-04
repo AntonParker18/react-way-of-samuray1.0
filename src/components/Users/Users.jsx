7a74c831c0e5ede53c1072cfde4c9386c5e54fbe
AntonParker18 <kozachok2805@gmail.com>
2022-02-04 16:00:30 +0200

    react-way-of-samuray 49.2: create users UI

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,82 @@
 import React from 'react'
+import s from './Users.module.css'
 
-const Users = () => {
-  return <div>Users</div>
+const Users = props => {
+
+  if(props.users.lenght === 0) {
+    props.setUsers([
+      {
+        id: 1,
+        photoUrl:
+          'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/428e2842-4157-45e8-a9af-1e5245e52c37/300x450',
+        followed: false,
+        fullName: 'Anton',
+        status: 'I am good',
+        lacation: { city: 'Vinnytsia', country: 'Ukraine' },
+      },
+      {
+        id: 2,
+        photoUrl:
+          'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/428e2842-4157-45e8-a9af-1e5245e52c37/300x450',
+        followed: true,
+        fullName: 'Zahar',
+        status: 'I am good',
+        lacation: { city: 'Moscow', country: 'Russia' },
+      },
+      {
+        id: 3,
+        photoUrl:
+          'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/428e2842-4157-45e8-a9af-1e5245e52c37/300x450',
+        followed: true,
+        fullName: 'Dmitry',
+        status: 'I am good',
+        lacation: { city: 'Minsk', country: 'Belarus' },
+      },
+    ])
+  }
+
+
+
+  return (
+    <div>
+      {props.users.map(u => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img src={u.photoUrl} className={s.usersPhoto} />
+            </div>
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id)
+                  }}
+                >
+                  Follow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id)
+                  }}
+                >
+                  Unfollow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <div>{u.fullName}</div>
+            <div>{u.status}</div>
+          </span>
+          <span>
+            <div>{u.lacation.country}</div>
+            <div>{u.lacation.city}</div>
+          </span>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Users
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,29 +3,7 @@ const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
 
 let initialState = {
-  users: [
-    // {
-    //   id: 1,
-    //   followed: false,
-    //   fullName: 'Anton',
-    //   status: 'I am good',
-    //   lacation: { city: 'Vinnytsia', country: 'Ukraine' },
-    // },
-    // {
-    //   id: 1,
-    //   followed: true,
-    //   fullName: 'Zahar',
-    //   status: 'I am good',
-    //   lacation: { city: 'Moscow', country: 'Russia' },
-    // },
-    // {
-    //   id: 1,
-    //   followed: true,
-    //   fullName: 'Dmitry',
-    //   status: 'I am good',
-    //   lacation: { city: 'Minsk', country: 'Belarus' },
-    // },
-  ],
+  users: []
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -61,3 +39,5 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = userId => ({ type: FOLLOW, userId })
 export const unfollowAC = userId => ({ type: UNFOLLOW, userId })
 export const setUsersAC = users => ({ type: SET_USERS, users })
+
+export default usersReducer
