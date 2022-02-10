8031aa5ac5df84226da9f4665ab28b454929137d
AntonParker18 <kozachok2805@gmail.com>
2022-02-10 23:56:36 +0200

    react-way-of-samuray 59.2: add description user

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,20 +2,49 @@ import React from 'react'
 import Preloader from '../../common/preloader'
 import s from './ProfileInfo.module.css'
 
-const ProfileInfo = (props) => {
-
-  if(!props.profile) {
+const ProfileInfo = props => {
+  if (!props.profile) {
     return <Preloader />
   }
+
+  const networks = Object.keys(props.profile.contacts).filter(
+    networkName => props.profile.contacts[networkName]
+  )
+
+    const lookingForAJob = props.profile.lookingForAJob ? 'Yes' : 'No'
 
   return (
     <div className={s.contant}>
       <div>
-        <img src='https://assets1.ignimgs.com/2020/11/14/spidermanremastered-blogroll-1605322159516_160w.jpg?width=1280' />
-      </div >
+        <img
+          className={s.wallpaper}
+          src='https://assets1.ignimgs.com/2020/11/14/spidermanremastered-blogroll-1605322159516_160w.jpg?width=1280'
+        />
+      </div>
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} />
-        ava + description</div>
+        ava + description
+        <div>About me: {props.profile.aboutMe}</div>
+        <div>Full name: {props.profile.fullName}</div>
+        <div>
+          Looking for a job:
+          <span> {lookingForAJob}</span>
+        </div>
+        <div>
+          Looking for a job description:
+          {props.profile.lookingForAJobDescription}
+        </div>
+        <div>
+          <div>
+            <h3>Contacts:</h3>
+            {networks.map(networkName => (
+              <div>
+                {networkName}: {props.profile.contacts[networkName]}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
