c12947ca1846be2511ce518056e1b24e5238eb7b
AntonParker18 <kozachok2805@gmail.com>
2022-02-14 01:00:57 +0200

    react-way-of-samuray 61: create user-button-login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import s from './Header.module.css'
 
-const Header = () => {
+const Header = props => {
   return (
     <header className={s.header}>
       <img src='https://assets.teenvogue.com/photos/618a84502c843cb59d7b962e/1:1/w_1997,h_1997,c_limit/171172352' />
+
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+      </div>
     </header>
   )
 }
