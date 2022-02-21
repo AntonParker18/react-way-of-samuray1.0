fd01d99641d52e53fec7356631fc64d8277734c0
AntonParker18 <kozachok2805@gmail.com>
2022-02-21 17:33:57 +0200

    react-way-of-samuray 75: install and use redux-form library

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { reduxForm, Field } from 'redux-form'
+
+const LoginFrom = props => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field placeholder='Login' name={'Login'} component={'input'} />
+      </div>
+      <div>
+        <Field placeholder='Password' name={'Password'} component={'input'} />
+      </div>
+      <div>
+        <Field component={'input'} type='checkbox' name={'rememberMe'} />{' '}
+        remember me
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  )
+}
+
+const LoginReduxFrom = reduxForm({ form: 'login' })(LoginFrom)
 
 const Login = () => {
-  return <h1>Login</h1>
+  const onSubmit = formData => {
+    console.log(formData)
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginReduxFrom onSubmit={onSubmit} />
+    </div>
+  )
 }
 
 export default Login
