4f4acce77fe60e6d2357330bb4a88e8dbd5f8d3a
AntonParker18 <kozachok2805@gmail.com>
2022-05-20 23:40:17 +0200

    react-way-of-samuray 91: add react-redux extantion

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import authReducer from './auth-reducer'
 import dialogsReduser from './dialogs-reducer'
 import profileReducer from './profile-reducer'
@@ -7,6 +7,7 @@ import usersReducer from './users-reducer'
 import thunkMiddleware from 'redux-thunk'
 import { reducer as formReducer } from 'redux-form'
 import appReducer from './app-reducer'
+import { reducer } from 'redux-form'
 
 let reducers = combineReducers({
   profilePage: profileReducer,
@@ -17,9 +18,12 @@ let reducers = combineReducers({
   form: formReducer,
   app: appReducer,
 })
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+)
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware))
-
-window.store = store
+window.__store__ = store
 
 export default store
